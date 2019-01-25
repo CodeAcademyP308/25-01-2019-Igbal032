2f847c0b114b765a5d28105575c346a7daeca128
Qasimovrufat <34399399+Qasimovrufat@users.noreply.github.com>
2019-01-25 21:15:44 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program prg = new Program();
+            Console.WriteLine("Fennlerin sayini daxil edin:");
+            var n = int.Parse(Console.ReadLine());
+            int[] grades = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(""+(i+1)+".fenni daxil edin:");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Ortalamaniz :"+ prg.CalculateAvarage(grades));
+
+        }
+        double CalculateAvarage(int[] grade)
+        {
+            double avarage;
+            int sum = 0;
+            for(int i = 0; i < grade.Length; i++)
+            {
+                sum += grade[i];
+            }
+            avarage = sum / grade.Length;
+
+            return avarage;
+        }
+     
+     
+    }
+}
